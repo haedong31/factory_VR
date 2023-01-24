c4178c60211a880b0e546665777146d0528279e3
Kim, Haedong <haedong.kim90@gmail.com>
2023-01-24 18:16:27 -0500

    Debug (Time frames)

--- figures.m
+++ figures.m
@@ -12,12 +12,17 @@ elist_assembly = readtable("./sim_data/assembly_line_4players8_elist.csv");
 cps_craft = graph_sim_cps(nlist_craft,elist_craft);
 cps_aseembly = graph_sim_cps(nlist_assembly,elist_assembly);
 
-figure('Color','w','Position',[0,0,1000,540])
+t1 = unique(elist_craft.t);
+t2 = unique(elist_assembly.t);
+ta = cps_aseembly;
+
+figure('Color','w','Position',[50,50,1000,540])
 tle = tiledlayout(2,3);
 xlabel(tle,"Time (s)", 'FontWeight','bold')
 
 nexttile
 plot(cps_craft(:,1,2),'LineWidth',1.5,'Color','red')
+xline(ta)
 hold on
 plot(cps_aseembly(:,1,2),'LineWidth',1.5,'Color','blue')
 xlim('tight')
@@ -268,12 +273,13 @@ end
 
 %% helper functions
 function out_mx = graph_sim_cps(nlist,elist)
+    tlist = unique(elist.t);
     nlist.Properties.VariableNames = {'Name','Label'};
     nlist.Name = string(nlist.Name);
     num_nodes = size(nlist,1);
-    num_edges = size(elist,1);
+    num_edges = length(tlist);
     num_players = (size(elist,2)-1)/2;
-
+    
     % initialize graphs
     G = cell(4,1);
     A = cell(4,1);
@@ -281,32 +287,40 @@ function out_mx = graph_sim_cps(nlist,elist)
         G{i} = digraph(zeros(num_nodes,num_nodes),nlist);
         A{i} = adjacency(G{i});
     end
-
+    
     out_mx = NaN(num_edges,nchoosek(num_players,2),2); % num_edges x 4C2 x 2 (adj_spectl,feat_sim)
     for i=1:num_edges
-        r = table2array(elist(i,:));
+        runningt = tlist(i);
+        r = table2array(elist(elist.t==runningt,:));
         gfeat = NaN(num_players,num_nodes,7);
-
-        for j=1:num_players
-            s = r(2*j);
-            t = r(2*j+1);
-
-            if (s==0)&&(t==0)
-                % no update 
-            elseif xor(s==0,t==0)
-                error("Invalid edge")
-            else
-                [edge_exist,update_idx] = ismember([s,t],str2double(G{j}.Edges.EndNodes),'rows');
-                if (all(edge_exist))
-                    w = G{j}.Edges.Weight;
-                    G{j}.Edges.Weight(update_idx) = w(update_idx)+1;
+        
+        % update graphs
+        for j=1:size(r,1)
+            for k=1:num_players
+                s = r(j,2*k);
+                t = r(j,2*k+1);
+        
+                if (s==0)&&(t==0)
+                    % no update 
+                elseif xor(s==0,t==0)
+                    error("Invalid edge")
                 else
-                    G{j} = addedge(G{j},num2str(s),num2str(t),1);
+                    [edge_exist,update_idx] = ismember([s,t],str2double(G{k}.Edges.EndNodes),'rows');
+                    if (all(edge_exist))
+                        w = G{k}.Edges.Weight;
+                        G{k}.Edges.Weight(update_idx) = w(update_idx)+1;
+                    else
+                        G{k} = addedge(G{k},num2str(s),num2str(t),1);
+                    end
                 end
             end
+        end
+        
+        % calculate graph features and CPS measure
+        for j=1:num_players
             % adjacency matrices for spectral distance
             A{j} = adjacency(G{j});
-
+    
             % graph features for feature-based distance        
             gfeat(j,:,1) = centrality(G{j},'outdegree','Importance',G{j}.Edges.Weight);
             gfeat(j,:,2) = centrality(G{j},'indegree','Importance',G{j}.Edges.Weight);
@@ -315,20 +329,20 @@ function out_mx = graph_sim_cps(nlist,elist)
             gfeat(j,:,5) = centrality(G{j},'betweenness');
             gfeat(j,:,6) = centrality(G{j},'pagerank','Importance',G{j}.Edges.Weight);
             gfeat(j,:,7) = centrality(G{j},'hubs','Importance',G{j}.Edges.Weight);
-        end
-        
-        % graph feature & similarity
-        l = 0;
-        for j=1:(num_players-1)
-            lambdaj = eigs(A{j});
-            n = 1+l;
-            for k=(j+1):(num_players)
+            
+            % graph feature & similarity
+            l = 0;
+            for k=1:(num_players-1)
                 lambdak = eigs(A{k});
-                out_mx(i,n,1) = norm(lambdaj-lambdak,2); % spectral
-                out_mx(i,n,2) = norm(gfeat(j,:,:)-gfeat(k,:,:),'fro'); % feature based
-                n = n+1;
+                n = 1+l;
+                for m=(k+1):(num_players)
+                    lambdam = eigs(A{m});
+                    out_mx(i,n,1) = norm(lambdak-lambdam,2); % spectral
+                    out_mx(i,n,2) = norm(gfeat(k,:,:)-gfeat(m,:,:),'fro'); % feature based
+                    n = n+1;
+                end
+                l = l+(num_players-k);
             end
-            l = l+(num_players-j);
         end
     end
 end
