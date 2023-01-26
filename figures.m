282aca7eef435b54c1af503557169b9bd8ffb1bc
Haedong Kim <haedong.kim90@gmail.com>
2023-01-26 15:44:28 -0500

    Figure setting

--- figures.m
+++ figures.m
@@ -1,4 +1,4 @@
-%% CPS assesment without edge-weight cut
+%% CPS assesment
 clc
 clearvars
 close all
@@ -91,189 +91,136 @@ grid on
 set(gca,'FontWeight','bold','LineWidth',1.5)
 hold off
 
-%% CPS assesment with edge-weight cut
+%% low-dimensional embedding for user-action space
 clc
-clearvars
 close all
-
-nlist = readtable("./sim_data/craft_4players8_nlist.csv");
-elist = readtable("./sim_data/craft_4players8_elist.csv");
-
-nlist.Properties.VariableNames = {'Name','Label'};
-nlist.Name = string(nlist.Name);
-num_nodes = size(nlist,1);
-num_edges = size(elist,1);
-
-edge_wcutoff = 16;
-num_players = (size(elist,2)-1)/2;
-
-% initialize graphs
-G = cell(4,1);
-A = cell(4,1);
-for i=1:num_players
-    G{i} = digraph(zeros(num_nodes,num_nodes),nlist);
-    A{i} = adjacency(G{i});
-end
-
-out_mx = NaN(num_edges,nchoosek(num_players,2),2); % num_edges x 4C2 x 2 (adj_spectl,feat_sim)
-for i=1:num_edges
-    r = table2array(elist(i,:));
-    gfeat = NaN(num_players,num_nodes,7);
-
-    for j=1:num_players
-        s = r(2*j);
-        t = r(2*j+1);
-
-        if (s==0)&&(t==0)
-            % no update 
-        elseif xor(s==0,t==0)
-            error("Invalid edge")
-        else
-            [edge_exist,update_idx] = ismember([s,t],str2double(G{j}.Edges.EndNodes),'rows');
-            if (all(edge_exist))
-                w = G{j}.Edges.Weight;
-                if (w(update_idx) >= edge_wcutoff)
-                    G{j}.Edges.Weight(update_idx) = w(update_idx)+(0.5)^(w(update_idx)-edge_wcutoff+1);
-                else
-                    G{j}.Edges.Weight(update_idx) = w(update_idx)+1;
-                end
-            else
-                G{j} = addedge(G{j},num2str(s),num2str(t),1);
-            end
-        end
-        % adjacency matrices for spectral distance
-        A{j} = adjacency(G{j});
-
-        % graph features for feature-based distance        
-        gfeat(j,:,1) = centrality(G{j},'outdegree','Importance',G{j}.Edges.Weight);
-        gfeat(j,:,2) = centrality(G{j},'indegree','Importance',G{j}.Edges.Weight);
-        gfeat(j,:,3) = centrality(G{j},'outcloseness');
-        gfeat(j,:,4) = centrality(G{j},'incloseness');
-        gfeat(j,:,5) = centrality(G{j},'betweenness');
-        gfeat(j,:,6) = centrality(G{j},'pagerank','Importance',G{j}.Edges.Weight);
-        gfeat(j,:,7) = centrality(G{j},'hubs','Importance',G{j}.Edges.Weight);
-    end
-    
-    % graph feature & similarity
-    l = 0;
-    for j=1:(num_players-1)
-        lambdaj = eigs(A{j});
-        n = 1+l;
-        for k=(j+1):(num_players)
-            lambdak = eigs(A{k});
-            out_mx(i,n,1) = norm(lambdaj-lambdak,2); % spectral
-            out_mx(i,n,2) = norm(gfeat(j,:,:)-gfeat(k,:,:),'fro'); % feature based
-            n = n+1;
-        end
-        l = l+(num_players-j);
-    end
-end
-
-figure('Color','w','Position',[0,0,1000,540])
-tle = tiledlayout(2,3);
-xlabel(tle,"Time (s)", 'FontWeight','bold')
-
-nexttile
-plot(out_mx(:,1,2),'LineWidth',1.5,'Color','black')
-ylim([0,60])
-ylabel("Distance (P1&P2)",'FontWeight','bold')
-
-nexttile
-plot(out_mx(:,2,2),'LineWidth',1.5,'Color','black')
-ylim([0,60])
-ylabel("Distance (P1&P3)",'FontWeight','bold')
-
-nexttile
-plot(out_mx(:,3,2),'LineWidth',1.5,'Color','black')
-ylim([0,60])
-ylabel("Distance (P1&P4)",'FontWeight','bold')
-
-nexttile
-plot(out_mx(:,4,2),'LineWidth',1.5,'Color','black')
-ylim([0,60])
-ylabel("Distance (P2&P3)",'FontWeight','bold')
-
-nexttile
-plot(out_mx(:,5,2),'LineWidth',1.5,'Color','black')
-ylim([0,60])
-ylabel("Distance (P2&P4)",'FontWeight','bold')
-
-nexttile
-plot(out_mx(:,6,2),'LineWidth',1.5,'Color','black')
-ylim([0,60])
-ylabel("Distance (P3&P4)",'FontWeight','bold')
-
-%% Dynamic graphs snapshots (4 times)
-clc
 clearvars
-close all
 
-nlist = readtable("./sim_data/assembly_line_4players8_nlist.csv");
-elist = readtable("./sim_data/assembly_line_4players8_elist.csv");
+nlist_craft = readtable("./sim_data/craft_4players8_nlist.csv");
+elist_craft = readtable("./sim_data/craft_4players8_elist.csv");
+nlist_assembly = readtable("./sim_data/assembly_line_4players8_nlist.csv");
+elist_assembly = readtable("./sim_data/assembly_line_4players8_elist.csv");
 
-nlist.Properties.VariableNames = {'Name','Label'};
-nlist.Name = string(nlist.Name);
-num_nodes = size(nlist,1);
-num_edges = size(elist,1);
+gfeat_craft = graph_feat(nlist_craft,elist_craft);
+gfeat_assembly = graph_feat(nlist_assembly,elist_assembly);
+acts = ["Spawn","Move","Assemble","Pass"];
 
-edge_wcutoff = 16;
-num_players = (size(elist,2)-1)/2;
+%----- craft production -----%
+dims = size(gfeat_craft);
+gfeat_craft2 = reshape(gfeat_craft,[dims(1)*dims(2)*dims(3),dims(4)]);
 
-% initialize graphs; snap graphs at 4 time points
-G = cell(4,1);
-Gsnaps = cell(4,4);
-for i=1:num_players
-    G{i} = digraph(zeros(num_nodes,num_nodes),nlist);
-end
-snap_pts = [floor(quantile(1:num_edges,0.25)),floor(quantile(1:num_edges,0.5)),...
-    floor(quantile(1:num_edges,0.75)),floor(quantile(1:num_edges,1))];
+% % label 1: category-level 1
+% l1 = repmat(unique(elist_craft.t),[dims(2)*dims(3),1]);
+% 
+% % label 2: category-level 2
+% l2_unit = strings(dims(1)*dims(2),1);
+% for i=1:dims(2)
+%     l2_unit(1+(i-1)*dims(1):i*dims(1)) = strcat("Player",num2str(i));
+% end
+% l2 = strings(dims(1)*dims(2)*dims(3),1);
+% for i=1:dims(3)
+%     l2(1+(i-1)*dims(1)*dims(2):i*dims(1)*dims(2)) = l2_unit;
+% end
 
-% have graphs evolve
-tpointer = 1;
-for i=1:num_edges
-    r = table2array(elist(i,:));
-    for j=1:num_players
-        s = r(2*j);
-        t = r(2*j+1);
-
-        if (s==0)&&(t==0)
-            % no update 
-        elseif xor(s==0,t==0)
-            error("Invalid edge")
-        else
-            [edge_exist,update_idx] = ismember([s,t],str2double(G{j}.Edges.EndNodes),'rows');
-            if (all(edge_exist))
-                w = G{j}.Edges.Weight;
-                if (w(update_idx) >= edge_wcutoff)
-                    G{j}.Edges.Weight(update_idx) = w(update_idx)+(0.5)^(w(update_idx)-edge_wcutoff+1);
-                else
-                    G{j}.Edges.Weight(update_idx) = w(update_idx)+1;
-                end
-            else
-                G{j} = addedge(G{j},num2str(s),num2str(t),1);
-            end
-        end
-    end
-
-    if i==snap_pts(tpointer)
-        for j=1:num_players
-            Gsnaps{j,tpointer} = G{j};
-        end
-        tpointer = tpointer+1;
-    end
+% label 3: category-level 3
+l3 = strings(dims(1)*dims(2)*dims(3),1);
+for i=1:dims(3)
+    l3(1+(i-1)*dims(1)*dims(2):i*dims(1)*dims(2)) = acts(i);
 end
 
-ccodes = parula(num_players);
-figure('Color','w','Position',[10,10,1200,1300])
-for i=1:num_players
-    for j=1:length(snap_pts)
-        subplot(4,4,num_players*(i-1)+j)
-        plot(Gsnaps{i,j},'NodeLabel',nlist.Label,'NodeColor',ccodes(i,:),'MarkerSize',10,...
-            'NodeFontSize',11,'NodeFontWeight','bold',...
-            'EdgeColor',ccodes(i,:),'LineWidth',Gsnaps{i,j}.Edges.Weight,'Layout','auto')
-        set(gca,'Visible','off')
-    end
+c = NaN(length(l3),3);
+idx1 = (l3=="Spawn");
+idx2 = (l3=="Move");
+idx3 = (l3=="Assemble");
+idx4 = (l3=="Pass");
+
+cmap_end = parula(4);
+cmap_begin = [0.7, 0.65, 1;
+    0.7, 0.87, 1;
+    0.7, 0.8, 0.6;
+    0.98, 0.99, 0.7];
+cmap = [linspace(cmap_begin(1,1),cmap_end(1,1),dims(1))', ...
+    linspace(cmap_begin(1,2),cmap_end(1,2),dims(1))', ...
+    linspace(cmap_begin(1,3),cmap_end(1,3),dims(1))'];
+c(idx1,:) = repmat(cmap,dims(2),1);
+cmap = [linspace(cmap_begin(2,1),cmap_end(2,1),dims(1))', ...
+    linspace(cmap_begin(2,2),cmap_end(2,2),dims(1))', ...
+    linspace(cmap_begin(2,3),cmap_end(2,3),dims(1))'];
+c(idx2,:) = repmat(cmap,dims(2),1);
+cmap = [linspace(cmap_begin(3,1),cmap_end(3,1),dims(1))', ...
+    linspace(cmap_begin(3,2),cmap_end(3,2),dims(1))', ...
+    linspace(cmap_begin(3,3),cmap_end(3,3),dims(1))'];
+c(idx3,:) = repmat(cmap,dims(2),1);
+cmap = [linspace(cmap_begin(4,1),cmap_end(4,1),dims(1))', ...
+    linspace(cmap_begin(4,2),cmap_end(4,2),dims(1))', ...
+    linspace(cmap_begin(4,3),cmap_end(4,3),dims(1))'];
+c(idx4,:) = repmat(cmap,dims(2),1);
+
+rng(231)
+ops = statset('MaxIter',2000);
+f = figure('Color','w','Position',[100,100,920,320]);
+orient(f,'landscape')
+
+subplot(1,2,1)
+embd = tsne(gfeat_craft2,'Distance','jaccard','NumDimensions',2, ...
+    'Standardize',true,'Options',ops);
+scatter(embd(idx1,1),embd(idx1,2),'o','filled','CData',c(idx1,:))
+hold on
+scatter(embd(idx2,1),embd(idx2,2),'square','filled','CData',c(idx2,:))
+scatter(embd(idx3,1),embd(idx3,2),'diamond','filled','CData',c(idx3,:))
+scatter(embd(idx4,1),embd(idx4,2),'^','filled','CData',c(idx4,:))
+hold off
+xlabel("Embedding Dim1")
+ylabel("Embedding Dim2")
+axis tight
+legend(acts)
+set(gca,'FontWeight','bold','LineWidth',1.5)
+
+%----- mass production -----%
+dims = size(gfeat_assembly);
+gfeat_assembly2 = reshape(gfeat_assembly,[dims(1)*dims(2)*dims(3),dims(4)]);
+
+l3 = strings(dims(1)*dims(2)*dims(3),1);
+for i=1:dims(3)
+    l3(1+(i-1)*dims(1)*dims(2):i*dims(1)*dims(2)) = acts(i);
 end
+
+c = NaN(length(l3),3);
+idx1 = (l3=="Spawn");
+idx2 = (l3=="Move");
+idx3 = (l3=="Assemble");
+idx4 = (l3=="Pass");
+
+cmap = [linspace(cmap_begin(1,1),cmap_end(1,1),dims(1))', ...
+    linspace(cmap_begin(1,2),cmap_end(1,2),dims(1))', ...
+    linspace(cmap_begin(1,3),cmap_end(1,3),dims(1))'];
+c(idx1,:) = repmat(cmap,dims(2),1);
+cmap = [linspace(cmap_begin(2,1),cmap_end(2,1),dims(1))', ...
+    linspace(cmap_begin(2,2),cmap_end(2,2),dims(1))', ...
+    linspace(cmap_begin(2,3),cmap_end(2,3),dims(1))'];
+c(idx2,:) = repmat(cmap,dims(2),1);
+cmap = [linspace(cmap_begin(3,1),cmap_end(3,1),dims(1))', ...
+    linspace(cmap_begin(3,2),cmap_end(3,2),dims(1))', ...
+    linspace(cmap_begin(3,3),cmap_end(3,3),dims(1))'];
+c(idx3,:) = repmat(cmap,dims(2),1);
+cmap = [linspace(cmap_begin(4,1),cmap_end(4,1),dims(1))', ...
+    linspace(cmap_begin(4,2),cmap_end(4,2),dims(1))', ...
+    linspace(cmap_begin(4,3),cmap_end(4,3),dims(1))'];
+c(idx4,:) = repmat(cmap,dims(2),1);
+
+subplot(1,2,2)
+embd = tsne(gfeat_assembly2,'Distance','jaccard','NumDimensions',2, ...
+    'Standardize',true,'Options',ops);
+scatter(embd(idx1,1),embd(idx1,2),'o','filled','CData',c(idx1,:))
+hold on
+scatter(embd(idx2,1),embd(idx2,2),'square','filled','CData',c(idx2,:))
+scatter(embd(idx3,1),embd(idx3,2),'diamond','filled','CData',c(idx3,:))
+scatter(embd(idx4,1),embd(idx4,2),'^','filled','CData',c(idx4,:))
+hold off
+xlabel("Embedding Dim1")
+ylabel("Embedding Dim2")
+axis tight
+set(gca,'FontWeight','bold','LineWidth',1.5)
 
 %% helper functions
 function out_mx = graph_sim_cps(nlist,elist)
@@ -347,6 +294,66 @@ function out_mx = graph_sim_cps(nlist,elist)
                 end
                 l = l+(num_players-k);
             end
+        end
+    end
+end
+
+function gfeat = graph_feat(nlist,elist)
+    tlist = unique(elist.t);
+    nlist.Properties.VariableNames = {'Name','Label'};
+    nlist.Name = string(nlist.Name);
+    num_nodes = size(nlist,1);
+    num_edges = length(tlist);
+    num_players = (size(elist,2)-1)/2;
+    
+    % initialize graphs
+    G = cell(4,1);
+    A = cell(4,1);
+    for i=1:num_players
+        G{i} = digraph(zeros(num_nodes,num_nodes),nlist);
+        A{i} = adjacency(G{i});
+    end
+    
+    gfeat = NaN(num_edges,num_players,num_nodes,7); % t x p x a x f
+    for i=1:num_edges
+        runningt = tlist(i);
+        r = table2array(elist(elist.t==runningt,:));
+        
+        % update graphs
+        for j=1:size(r,1)
+            for k=1:num_players
+                s = r(j,2*k);
+                t = r(j,2*k+1);
+        
+                if (s==0)&&(t==0)
+                    % no update 
+                elseif xor(s==0,t==0)
+                    error("Invalid edge")
+                else
+                    [edge_exist,update_idx] = ismember([s,t],str2double(G{k}.Edges.EndNodes),'rows');
+                    if (all(edge_exist))
+                        w = G{k}.Edges.Weight;
+                        G{k}.Edges.Weight(update_idx) = w(update_idx)+1;
+                    else
+                        G{k} = addedge(G{k},num2str(s),num2str(t),1);
+                    end
+                end
+            end
+        end
+        
+        % calculate graph features and CPS measure
+        for j=1:num_players
+            % adjacency matrices for spectral distance
+            A{j} = adjacency(G{j});
+
+            % graph features for feature-based distance        
+            gfeat(i,j,:,1) = centrality(G{j},'outdegree','Importance',G{j}.Edges.Weight);
+            gfeat(i,j,:,2) = centrality(G{j},'indegree','Importance',G{j}.Edges.Weight);
+            gfeat(i,j,:,3) = centrality(G{j},'outcloseness');
+            gfeat(i,j,:,4) = centrality(G{j},'incloseness');
+            gfeat(i,j,:,5) = centrality(G{j},'betweenness');
+            gfeat(i,j,:,6) = centrality(G{j},'pagerank','Importance',G{j}.Edges.Weight);
+            gfeat(i,j,:,7) = centrality(G{j},'hubs','Importance',G{j}.Edges.Weight);  
         end
     end
 end
